b2c5adcb7d2fa6f76caf5af3819ed26b3f47ebcc
Paulo <paulo_sanches2012@hotmail.com>
2020-04-08 14:25:13 -0300

    aplicação mobile Be The Hero

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import 'intl'; //Colocar valores em reais
+import 'intl/locale-data/jsonp/pt-BR'; //Colocar valores em reais
+import Routes from './src/routes';
+
+export default function App() {
+  return (
+    <Routes />
+  );
+}
